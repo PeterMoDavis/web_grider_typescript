73b9ee60890f22bbe431180e36535d28f7ae68bc
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-29 13:09:05 -0400

    added dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import { User } from "./models/User";
 
 const user = new User({ name: "myname", age: 20 });
 
-user.set({ name: "newname" });
+user.on("change", () => {
+  console.log("change #1");
+});
+user.on("change", () => {
+  console.log("change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggered");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.trigger("fjdjsfjdsll");
+console.log(user.events.change);
