386249404e77d4923fb8182a96de6a35727adede
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-06-30 17:51:36 -0400

    added model

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,4 +1,4 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise, AxiosResponse } from 'axios';
 
 interface ModelAttributes<T> {
   set(value: T): void;
@@ -6,7 +6,11 @@ interface ModelAttributes<T> {
   get<K extends keyof T>(key: K): T[K];
 }
 
-interface Sync<T> {
+interface HasId {
+  id?: number;
+}
+
+interface Sync<T extends HasId> {
   fetch(id: number): AxiosPromise;
   save(data: T): AxiosPromise;
 }
@@ -16,4 +20,46 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-export class Model {}
+export class Model<T> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
+}
