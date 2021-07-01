f999a23dd45086555c1129caae03fbb4c6e372e3
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-07-01 12:02:31 -0400

    finalized Collections

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,15 @@
-import { Model } from './Model';
-import { Attributes } from './Attributes';
-import { ApiSync } from './ApiSync';
-import { Eventing } from './Eventing';
+import { Model } from "./Model";
+import { Attributes } from "./Attributes";
+import { ApiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
+import { Collection } from "./Collection";
 export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-const rootUrl = 'http://localhost:3000/users';
+const rootUrl = "http://localhost:3000/users";
 
 export class User extends Model<UserProps> {
   static buildUser(attrs: UserProps): User {
@@ -16,6 +17,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+  static buildUserCollection(): Collection<User, UserProps> {
+    new Collection<User, UserProps>(
+      "http://localhost:3000/users",
+      (json: UserProps) => User.buildUser(json)
     );
   }
 }
